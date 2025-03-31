843666b23f8b832d9bc5d33386e0953c59965a0e
James Ash <thejamesash1@gmail.com>
2025-03-31 03:29:46 -0400

    [react] add dev files for making pages

---
+++ React/pages/lobby_dev.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LobbyDev: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Lobby</h1>
+            <p>This is the development version of the lobby page.</p>
+        </div>
+    );
+};
+
+export default LobbyDev;
---
+++ React/pages/prompt_dev.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PromptDev: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Prompt page</h1>
+            <p>This is the development version of the lobby page.</p>
+        </div>
+    );
+};
+
+export default PromptDev;
