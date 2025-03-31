62837156ad57e1ea6555b9fbc195b2c9294a96a4
James Ash <thejamesash1@gmail.com>
2025-03-31 01:42:41 -0400

    [react] start working on homepage

--- React/pages/index.tsx
+++ React/pages/index.tsx
@@ -1,9 +1,10 @@
+import NavigationBar from "@/components/NavigationBar";
 import classes from "../styles/index.module.css";
 
 export default function index() {
   return (
-    <div>
-        <h1>Test</h1>
+    <div className={classes.container}>
+        <NavigationBar />
     </div>
   );
 }
