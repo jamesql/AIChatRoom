614987a88ee33db6e0729a3319b0b3ec03fad9ca
James Ash <thejamesash1@gmail.com>
2025-04-14 17:05:27 -0400

    Add random bio generation to DashUserCard and handle avatar image errors

--- React/components/DashUserCard.tsx
+++ React/components/DashUserCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from "../styles/dashusercard.module.css";
+import { randomBio } from '@/util/randomMsgs';
 
 interface DashUserCardProps {
     name: string;
@@ -11,14 +12,20 @@ const DashUserCard: React.FC<DashUserCardProps> = ({ name, avatarUrl }) => {
         <div className={classes.dash_user_card}>
             <div className={classes.avatar}>
                 {avatarUrl ? (
-                    <img src={avatarUrl} alt={`${name}'s avatar`} />
+                    <img src={avatarUrl} alt={`${name}'s avatar`} onError={(e) => {
+                        const target = e.target as HTMLImageElement;
+                        target.onerror = null; // prevents looping
+                        target.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWVweXViZDdra24xdmNubTBxZmNjY2p3bzJucGRxajdoZjRybnB1ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7VzgMsB6FLCilwS30v/giphy.gif";
+                        target.alt = "Default avatar";
+
+                    }} />
                 ) : (
                     <div className="placeholder-avatar">{name.charAt(0).toUpperCase()}</div>
                 )}
             </div>
             <div className={classes.user_info}>
                 <h1>Welcome, {name}!</h1>
-                <p>Lorem ipsum</p>
+                <p>{randomBio()}</p>
             </div>
             <button>
                 <a href="/play">Join a Lobby</a>
