182bbfb6bee604b1cf2f346d5ed8b0fac21041fd
James Ash <thejamesash1@gmail.com>
2025-04-15 01:17:43 -0400

    Fix environment variable reference for API URL in ApiClient

--- React/util/api.ts
+++ React/util/api.ts
@@ -8,7 +8,7 @@ class ApiClient {
     private baseUrl: string;
 
     constructor() {
-        this.baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:80';
+        this.baseUrl = process.env.REACT_API_URL || 'http://localhost:80';
         this.axiosInstance = axios.create({
             baseURL: this.baseUrl,
             headers: {
