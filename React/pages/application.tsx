1b7c847add653a81c18ac424fe23b3593d1a434d
James Ash <thejamesash1@gmail.com>
2025-04-15 01:50:45 -0400

    Remove unused environment configuration import from Application component

--- React/pages/application.tsx
+++ React/pages/application.tsx
@@ -16,7 +16,6 @@ import VotingDev from './voting_dev';
 import LoseComponent from './lose';
 import WinComponent from './win';
 import VotedOut from './votedOut';
-import '../envConfig.ts';
 
 
 const Application: React.FC = () => {
